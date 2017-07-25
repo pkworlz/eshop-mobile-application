3b0be5146403f12adf743db1c10d59302973f43b
dotnetdreamer <dotnetdreamer@users.noreply.github.com>
2017-07-25 22:18:14 +0400

    dynamic ionic slider

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { NavController } from 'ionic-angular';
+
+import { SliderPage } from '../slider/slider.page';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  @ViewChild(SliderPage) sliderPage: SliderPage;
 
   constructor(public navCtrl: NavController) {
-
+    console.log('first time fires');
   }
 
+  ionViewWillEnter() {
+    console.log('call me everytime');
+    this.sliderPage.ionViewWillEnter()
+  }
 }
--- src/pages/slider/slider.page.ts
+++ src/pages/slider/slider.page.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { Slides } from 'ionic-angular';
 
 import { SliderService } from './slider.service';
 
@@ -8,11 +9,21 @@ import { SliderService } from './slider.service';
     providers: [SliderService]
 })
 export class SliderPage {
+    @ViewChild(Slides) slider: Slides;
+    public slides: any;
 
     constructor(private sliderService: SliderService) {
+    }
+
+    ionViewWillEnter() {
         this.sliderService.getSlides()
             .subscribe(slides => {
                 console.log(slides);
-            })
+                this.slides = slides;
+            });
+        setTimeout(() => {
+            this.slider.autoplay = 4000;
+            this.slider.startAutoplay();
+        },300);
     }
 }
